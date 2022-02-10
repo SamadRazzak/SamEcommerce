80858f5f05c208de0be085290ed44baf4a58ff72
Samad Razzak <Samad.Razzak@avanza.pk>
2022-02-10 15:27:00 +0500

    Initial Commit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,38 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 1,
+                Name = "Product One",            
+            });
+
+             modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 2,
+                Name = "Product Two",            
+            });
+
+             modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 3,
+                Name = "Product Three",            
+            });            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
